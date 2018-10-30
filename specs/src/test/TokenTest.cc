bc57a9f8bec4958bbe941542cfed96af0d62ac7e
Tali Yoavetali Yoav Nir <yoavetali@Talis-iMac.local>
2018-10-30 21:43:55 +0200

    Convert tokenTest to a unit test
    Signed-off-by: Tali Yoavetali Yoav Nir <yoavetali@Talis-iMac.local>

--- specs/src/test/TokenTest.cc
+++ specs/src/test/TokenTest.cc
@@ -1,15 +1,81 @@
 #include <iostream>
+#include "assert.h"
+#include "string.h"
 #include "cli/tokens.h"
 
-int main(int argc, char** argv)
+std::string callParseTokens(int argc, char** argv)
 {
-	std::vector<Token> vec = parseTokens(argc-1, argv+1); // skipping the program name
+	std::vector<Token> vec = parseTokens(argc, argv); // skipping the program name
 	normalizeTokenList(&vec);
 	
+	std::string ret;
+
 	for (int i=0; i<vec.size(); i++) {
-		int digits = (std::to_string(i)).length();
-		std::cout << i << ". " << vec[i].Debug(digits) << "\n";
+		ret += vec[i].Debug(0);
+		if (vec.size()>1 && i<vec.size()-1) {
+			ret += '|';
+		}
+	}
+
+	return ret;
+}
+
+std::string callParseTokens(const char* _str)
+{
+	int argc = 0;
+	char* argv[200];
+	static char* str = NULL;
+	if (str) free(str);
+	str = strdup(_str);
+	char* ptr = strtok(str, " ");
+	while (ptr) {
+		argv[argc++] = ptr;
+		ptr = strtok(NULL, " ");
+		assert(argc<200);
 	}
 	
-	return 0;
+	return callParseTokens(argc, argv);
 }
+
+bool checkParsing(const char* _str, const char* _expected, bool bSingle)
+{
+	static unsigned int ctr = 0;
+	std::string parsed;
+	if (bSingle) {
+		char* str = strdup(_str);
+		parsed = callParseTokens(1, &str);
+		free(str);
+	} else {
+		parsed = callParseTokens(_str);
+	}
+
+	std::cout << "Test " << ++ctr;
+	if (bSingle) std::cout << "(s)";
+	if (parsed==_expected) {
+		std::cout << ": *** OK ***  in=<" << _str << ">  out=" << parsed << "\n";
+		return true;
+	} else {
+		std::cout << ": * NOT-OK *  in=<" << _str << ">  out=" << parsed << " expected=" << _expected << "\n";
+		return false;
+	}
+}
+
+#define TESTNS(t,e) if (false==checkParsing(t,e,false)) failedTests++;
+int main(int argc, char** argv)
+{
+	unsigned int failedTests = 0;
+
+	TESTNS("w1 1","WORDRANGE; S:1|RANGE; S:1");
+	TESTNS("1 2 1-3 4;-2 -2;-1 *-7 *;-2 1-*", "RANGE; S:1|RANGE; S:2|RANGE; S:1-3|RANGE; S:4--2|RANGE; S:-2--1|LITERAL; /*-7/|LITERAL; /*;-2/|RANGE; S:1-end");
+	TESTNS("w1 word 2 word 2-3 word 2;-4 word3 wor 5", "WORDRANGE; S:1|WORDRANGE; S:2|WORDRANGE; S:2-3|WORDRANGE; S:2--4|LITERAL; /word3/|LITERAL; /wor/|RANGE; S:5");
+
+	if (failedTests) {
+		std::cout << "\n" << failedTests << " failed tests.\n";
+	} else {
+		std::cout << "\nAll tests passed.\n";
+	}
+
+	return (failedTests==0);
+}
+
+
