cfe92fb6a17aea54cecbc1efd6a1506a1353e828
Yoav Nir <yoav.nir@gmail.com>
2018-11-25 22:48:51 +0200

    Added AluUnit and two subclasses

--- specs/src/utils/alu.cc
+++ specs/src/utils/alu.cc
@@ -8,6 +8,7 @@
  */
 
 #include <sstream>
+#include "ErrorReporting.h"
 #include "alu.h"
 
 void ALUCounter::set(std::string& s)
@@ -37,23 +38,98 @@ void ALUCounter::set(ALUFloat f)
 	m_type = counterType__Float;
 }
 
-ALUInt ALUCounter::getInt()
+ALUInt ALUCounter::getInt() const
 {
 	return (counterType__None==m_type) ? 0 : std::stoll(m_value);
 }
 
-ALUInt ALUCounter::getHex()
+ALUInt ALUCounter::getHex() const
 {
 	return (counterType__None==m_type) ? 0 : std::stoll(m_value, NULL, 16);
 }
 
-ALUFloat ALUCounter::getFloat()
+ALUFloat ALUCounter::getFloat() const
 {
 	return (counterType__None==m_type) ? 0.0 : std::stold(m_value);
 }
 
-bool ALUCounter::isWholeNumber()
+bool ALUCounter::getBool() const
+{
+	return (m_value=="1");
+}
+
+bool ALUCounter::isWholeNumber() const
 {
 	return (ALUFloat(getInt())==getFloat());
 }
 
+unsigned int getCountOperands(AluUnitType t)
+{
+	switch (t) {
+	case UT_LiteralNumber:
+	case UT_Counter:
+	case UT_FieldIdentifier:
+		return 0;
+	case UT_UnaryOp:
+	case UT_AssignmentOp:
+		return 1;
+	case UT_BinaryOp:
+	case UT_RelOp:
+		return 2;
+	default:
+	{
+		std::string err = "Invalid unit type: " + std::to_string(t);
+		MYTHROW(err);
+	}
+	}
+}
+
+ALUCounter* AluUnit::compute()
+{
+	std::string err = _identify() + " should not be called with no operands";
+	MYTHROW(err);
+}
+
+ALUCounter* AluUnit::compute(ALUCounter* op)
+{
+	std::string err = _identify() + " should not be called with one operand";
+	MYTHROW(err);
+}
+
+ALUCounter* AluUnit::compute(ALUCounter* op1, ALUCounter* op2)
+{
+	std::string err = _identify() + " should not be called with two operands";
+	MYTHROW(err);
+}
+
+
+void AluUnitLiteral::_serialize(std::ostream& os) const
+{
+	os << '(' << m_literal.getFloat() << ')';
+}
+
+std::string AluUnitLiteral::_identify()
+{
+	return std::string("A literal (") + m_literal.getStr() + ")";
+}
+
+ALUCounter* AluUnitLiteral::compute()
+{
+	return new ALUCounter(m_literal);
+}
+
+
+void AluUnitCounter::_serialize(std::ostream& os) const
+{
+	os << '#' << m_ctrNumber;
+}
+
+std::string AluUnitCounter::_identify()
+{
+	return std::string("A counter (") + std::to_string(m_ctrNumber) + ")";
+}
+
+ALUCounter* AluUnitCounter::compute()
+{
+	return new ALUCounter(*(m_ctrs->getPointer(m_ctrNumber)));
+}
--- specs/src/utils/alu.h
+++ specs/src/utils/alu.h
@@ -2,6 +2,7 @@
 #define SPECS2016__UTILS__ALU__H
 
 #include <map>
+#include <vector>
 #include <string>
 
 typedef long long int ALUInt;
@@ -23,14 +24,15 @@ public:
 	ALUCounter(ALUFloat f)     {set(f);}
 	ALUCounterType getType()   {return m_type;}
 	std::string    getStr()    {return m_value;}
-	ALUInt         getInt();
-	ALUInt         getHex();
-	ALUFloat       getFloat();
+	ALUInt         getInt() const;
+	ALUInt         getHex() const;
+	ALUFloat       getFloat() const;
+	bool           getBool() const;
 	void           set(std::string& s);
 	void           set(const char* st);
 	void           set(ALUInt i);
 	void           set(ALUFloat f);
-	bool           isWholeNumber();
+	bool           isWholeNumber() const;
 private:
 	std::string    m_value;
 	ALUCounterType m_type;
@@ -38,10 +40,12 @@ private:
 
 class ALUCounters {
 public:
-	std::string    getStr(unsigned int i)   {return m_map[i].getStr();}
-	ALUInt         getInt(unsigned int i)   {return m_map[i].getInt();}
-	ALUInt         getHex(unsigned int i)   {return m_map[i].getHex();}
-	ALUFloat       getFloat(unsigned int i) {return m_map[i].getFloat();}
+	std::string    getStr(unsigned int i)		{return m_map[i].getStr();}
+	ALUInt         getInt(unsigned int i)		{return m_map[i].getInt();}
+	ALUInt         getHex(unsigned int i)		{return m_map[i].getHex();}
+	ALUFloat       getFloat(unsigned int i)		{return m_map[i].getFloat();}
+	bool           getBool(unsigned int i)		{return m_map[i].getBool();}
+	ALUCounter*    getPointer(unsigned int i)	{return &m_map[i];}
 	void           set(unsigned int i, std::string& s)  {m_map[i].set(s);}
 	void           set(unsigned int i, const char* st)  {m_map[i].set(st);}
 	void           set(unsigned int i, ALUInt l)        {m_map[i].set(l);}
@@ -51,5 +55,104 @@ public:
 private:
 	std::map<unsigned int, ALUCounter> m_map;
 };
+
+#define ALU_UOP_LIST	\
+	X(Plus,"+")		\
+	X(Minus,"-")	\
+	X(Not,"!")		\
+
+#define ALU_BOP_LIST	\
+	X(Add,"+")		\
+	X(Sub,"-")		\
+	X(Mult,"*")		\
+	X(Div,"/")  	\
+	X(IntDiv,"%")	\
+	X(RemDev,"//")	\
+	X(Appnd,"||")	\
+
+#define ALU_ASSOP_LIST \
+	X(Let,":=")		\
+	X(Add,"+=")		\
+	X(Sub,"-=")		\
+	X(Mult,"*=")	\
+	X(Div,"/=")		\
+	X(IntDiv,"%=")	\
+	X(RemDiv,"//=")	\
+	X(Appnd,"||=")	\
+
+#define ALU_RELOP_LIST	\
+	X(LT,"<")		\
+	X(LE,"<=")		\
+	X(GT,">")		\
+	X(GE,">=")		\
+	X(SLT,"<<")		\
+	X(SLTE,"<<=")	\
+	X(SGT,">>")		\
+	X(SGTE,">>=")	\
+	X(EQ,"=")		\
+	X(SEQ,"==")		\
+	X(NE,"!=")		\
+	X(SNE,"!==")	\
+	X(AND,"&")		\
+	X(OR,"|")		\
+
+enum AluUnitType {
+	UT_Invalid,
+	UT_LiteralNumber,
+	UT_Counter,
+	UT_FieldIdentifier,
+	UT_UnaryOp,
+	UT_BinaryOp,
+	UT_RelOp,
+	UT_AssignmentOp,
+};
+
+unsigned int getCountOperands(AluUnitType t);
+
+// This abstract class represents anything in an expression - a number, a counter,
+// a field identifier, an operation
+class AluUnit {
+public:
+	AluUnit()			{}
+	virtual ~AluUnit()	{}
+	virtual void   			_serialize(std::ostream& os) const = 0;
+	virtual std::string     _identify() = 0;
+	virtual AluUnitType		type()	{return UT_Invalid;}
+	virtual ALUCounter*		compute();
+	virtual ALUCounter*		compute(ALUCounter* op);
+	virtual ALUCounter*		compute(ALUCounter* op1, ALUCounter* op2);
+};
+
+class AluUnitLiteral : public AluUnit {
+public:
+	AluUnitLiteral(std::string& s):m_literal(s)	{}
+	virtual ~AluUnitLiteral()					{}
+	virtual void				_serialize(std::ostream& os) const;
+	virtual std::string			_identify();
+	virtual AluUnitType			type()			{return UT_LiteralNumber;}
+	virtual ALUCounter*			compute();
+private:
+	ALUCounter	m_literal;
+};
+
+class AluUnitCounter : public AluUnit {
+public:
+	AluUnitCounter(unsigned int ctrNumber, ALUCounters* ctrs):m_ctrs(ctrs),m_ctrNumber(ctrNumber) {};
+	~AluUnitCounter()			{}
+	virtual void				_serialize(std::ostream& os) const;
+	virtual std::string			_identify();
+	virtual AluUnitType			type()			{return UT_Counter;}
+	virtual ALUCounter*			compute();
+private:
+	ALUCounters* m_ctrs;
+	unsigned int m_ctrNumber;
+};
+
+static std::ostream& operator<< (std::ostream& os, const AluUnit &u)
+{
+    u._serialize(os);
+
+    return os;
+}
 
 #endif
