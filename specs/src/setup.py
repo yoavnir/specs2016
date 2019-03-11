6837498389cc9898cbc86d2ad61a28c42b7d5a26
synp71 <yoav.nir@gmail.com>
2019-03-12 00:00:04 +0200

    Issue #69: No compilation output to console

--- specs/src/setup.py
+++ specs/src/setup.py
@@ -213,7 +213,7 @@ else:
 	cppflags_test = "--std=c++11"
 
 if compiler!="VS":
-	test_put_time_cmd = "{} {} -o xx.o -c xx.cc".format(cxx,cppflags_test)
+	test_put_time_cmd = "{} {} -o xx.o -c xx.cc 2> /dev/null".format(cxx,cppflags_test)
 	with open("xx.cc", "w") as testfile:
 		testfile.write('#include <iomanip>\nvoid x() { std::put_time(NULL,""); }\n')
 	sys.stdout.write("Testing std::put_time()...")
@@ -226,7 +226,7 @@ if compiler!="VS":
 	if platform=="NT":
 		os.system("del xx.cc xx.o")
 	else:
-		os.system("/bin/rm xx.cc xx.o")
+		os.system("/bin/rm xx.cc xx.o 2> /dev/null")
 else:
 	CFG_put_time = True
 	
