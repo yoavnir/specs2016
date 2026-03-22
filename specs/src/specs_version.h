8a6f64763dd15dc911e86e8ab8ef7026f842f9aa
Yoav Nir <yoav.nir@gmail.com>
2026-03-22 17:11:10 +0200

    Issue #332 - add metadata to Windows executable (#333)
    * Also fix platform string for Windows

---
+++ specs/src/specs_version.h
@@ -0,0 +1,5 @@
+#define SPEC_PRODUCT_NAME "specs"
+#define SPEC_FILE_DESCRIPTION "A re-writing of the specs pipeline stage from CMS, only changed quite a bit"
+#define SPEC_PRODUCT_VERSION "0.9.9"
+#define SPEC_COPYRIGHT "Copyright (c) 2018-2026 Yoav Nir"
+#define SPEC_FILE_VERSION 0,9,9,0
