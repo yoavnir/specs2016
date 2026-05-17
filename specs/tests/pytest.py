bcd94e77c384b19ac3355bf62bd3d2a6536e9fbe
niry1 <Yoav.Nir@emc.com>
2026-05-17 14:01:29 +0300

    Remove extraneous debugging

--- specs/tests/pytest.py
+++ specs/tests/pytest.py
@@ -1,48 +1,37 @@
 import os,sys,subprocess
 
-# Change to the tests directory so relative paths work correctly
-# os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
 def run_cmd(spec, force=False):
 	args = ["../exe/specs", "--set", "SPECSPATH=/tmp", "-o", "theout"]
 	if force:
 		args.extend(["--pythonFuncs", "on"])
 	args.append(spec)
 	
-	# sys.stdout.write("\nCommand: {}\n".format(" ".join(args)))
-	# sys.stdout.write("CWD: {}\n".format(os.getcwd()))
-	# sys.stdout.write("../exe/specs exists: {}\n".format(os.path.exists("../exe/specs")))
-	# sys.stdout.write("Files before: theout={}, theerr={}\n".format(os.path.exists("theout"), os.path.exists("theerr")))
-	
 	with open("theerr", "w") as err_file:
 		rc = subprocess.call(args, stdout=subprocess.DEVNULL, stderr=err_file)
 	
-	# sys.stdout.write("Files after: theout={}, theerr={}\n".format(os.path.exists("theout"), os.path.exists("theerr")))
 	if os.path.exists("theerr"):
 		with open("theerr", "r") as f:
-			theerr_content = f.read()
-		# sys.stdout.write("theerr content (len={}): {}\n".format(len(theerr_content), repr(theerr_content)))
-	# sys.stdout.write("\n{}\n".format(spec))
-	# sys.stdout.write("Result: rc={}...theout={}...theerr={}...".format(rc,os.path.exists("theout"),os.path.exists("theerr")))
-	# subprocess.call returns actual exit code, not os.system format
-	# os.system returns exit_code << 8, so 8 becomes 2048
-	if rc!=0 and rc!=8:
-		# sys.stdout.write("not 0/8...")
-		ret = "RC="+str(rc)
-	elif rc==0 and os.path.exists("theout"):
-		with open("theout","r") as out:
-			ret = out.read()
-		os.system("/bin/rm theout")
-	elif os.path.exists("theerr"):
-		with open("theerr","r") as err:
-			errbuf = err.readlines()
-			if len(errbuf) > 0:
-				ret = errbuf[-1]
-			else:
-				ret = "(empty)"
+			theerr_content = f.readlines()
 		os.system("/bin/rm theerr")
 	else:
+		theerr_content = ""
+
+	if os.path.exists("theout"):
+		with open("theout", "r") as f:
+			theout_content = f.read()
+		os.system("/bin/rm theout")
+	else:
+		theout_content = ""
+
+	if rc!=0 and rc!=8:
+		ret = "RC="+str(rc)
+	elif rc==0 and theout_content != "":
+		ret = theout_content
+	elif theerr_content != "":
+		ret = theerr_content[-1]
+	else:
 		ret = "something happened"
+
 	return ret.strip()
 	
 def set_localfuncs(lf):
