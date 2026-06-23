c75faf89d62b833f847c4329895f727edd0a7b4e
Yoav Nir <yoav.nir@gmail.com>
2026-06-23 17:07:31 +0300

    Issue #449 - Fix a memory leak in pretty, and also (#450)
    Fix unit tests (ALUUnitTest and ProcessingTest) so that all
    tests pass when run one-by-one. Also, fix valgrind_unit_test
    so that the only acceptable failures are those caused by valgrind
    issue with long double.

--- specs/tests/valgrind_unit_tests.py
+++ specs/tests/valgrind_unit_tests.py
@@ -1,4 +1,4 @@
-import sys, memcheck, argparse
+import sys, memcheck, argparse, platform
 
 count_ALU_tests = 844
 count_processing_tests = 277
@@ -15,8 +15,12 @@ if args.nvg==True:
 	memcheck.no_valgrind = True
 all_tests_may_fail = True if args.okfail else False
 
+# Find machine type
+arch = platform.machine()
+
 tests_to_skip = []
-tests_that_may_fail = []
+tests_that_may_fail = [9, 43, 48, 61, 62, 63, 68, 69, 70, 71, 73, 74, 83, 151, 152, 153, 154, 188, 197, 198, 199, 211, 403, 404, 405, 406, 409, 411, 413, 414, 415, 416, 417, 418, 421, 423, 426, 428, 429, 430, 483, 737, 817] if arch=='x86_64' else []
+tests_that_failed = []
 for i in range(count_ALU_tests):
     if (i+1) in tests_to_skip:
         continue
@@ -28,20 +32,29 @@ for i in range(count_ALU_tests):
                 sys.stdout.write("ALUUnitTest Test #{} - valgrind not run, but it would have failed\n".format(i+1))
                 memcheck.cleanup_valgrind()
             else:
-                sys.stdout.write("ALUUnitTest Test #{} - no leaks (but it should have failed -- check out cmd.out)\n".format(i+1))
                 memcheck.cleanup_valgrind()
-                exit(0)
+                if all_tests_may_fail:
+                    sys.stdout.write("ALUUnitTest Test #{} - no leaks\n".format(i+1))
+                else:
+                    sys.stdout.write("ALUUnitTest Test #{} - no leaks (but it should have failed -- check out cmd.out)\n".format(i+1))
+                    exit(0)
         else:
             sys.stdout.write("ALUUnitTest Test #{} - no leaks\n".format(i+1))
     elif rc==memcheck.RetCode_COMMAND_FAILED and (i+1) in tests_that_may_fail or all_tests_may_fail:
-        sys.stdout.write("ALUUnitTest Test #{} - no leaks (but test failed)\n".format(i+1))
+        with open("cmd.out", "r") as f:
+            cmdout = f.read().strip()
+        sys.stdout.write("ALUUnitTest Test #{} - no leaks (but test failed)\n{}\n".format(i+1, cmdout))
+        tests_that_failed.append(i+1)
     else:
         sys.stdout.write("Command '{}' failed with return code '{}' and extra info {}\nSee file valgrind.out for details.\n".format(cmd,memcheck.RetCode_strings[rc], info))
         exit(0)
     memcheck.cleanup()
+if len(tests_that_failed) > 0:
+    sys.stdout.write("ALUUnitTest: {} tests failed: {}\n".format(len(tests_that_failed), tests_that_failed))
 
 
 tests_that_may_fail = []
+tests_that_failed = []
 for i in range(count_token_tests):
     cmd = "../exe/TokenTest {}".format(i+1)
     (rc,info) = memcheck.leak_check(cmd)
@@ -57,14 +70,20 @@ for i in range(count_token_tests):
         else:
             sys.stdout.write("TokenTest Test #{} - no leaks\n".format(i+1))
     elif rc==memcheck.RetCode_COMMAND_FAILED and (i+1) in tests_that_may_fail or all_tests_may_fail:
-        sys.stdout.write("TokenTest Test #{} - no leaks (but test failed)\n".format(i+1))
+        with open("cmd.out", "r") as f:
+            cmdout = f.read().strip()
+        sys.stdout.write("TokenTest Test #{} - no leaks (but test failed)\n{}\n".format(i+1, cmdout))
+        tests_that_failed.append(i+1)
     else:
         sys.stdout.write("Command '{}' failed with return code '{}' and extra info {}\nSee file valgrind.out for details.\n".format(cmd,memcheck.RetCode_strings[rc], info))
         exit(0)
     memcheck.cleanup()
+if len(tests_that_failed) > 0:
+    sys.stdout.write("TokenTest: {} tests failed: {}\n".format(len(tests_that_failed), tests_that_failed))
 
 
-tests_that_may_fail = [102]
+tests_that_may_fail = [73, 102, 103, 179] if arch=='x86_64' else []
+tests_that_failed = []
 for i in range(count_processing_tests):
     cmd = "../exe/ProcessingTest {}".format(i+1)
     (rc,info) = memcheck.leak_check(cmd)
@@ -80,8 +99,13 @@ for i in range(count_processing_tests):
         else:
             sys.stdout.write("ProcessingTest Test #{} - no leaks\n".format(i+1))
     elif rc==memcheck.RetCode_COMMAND_FAILED and (i+1) in tests_that_may_fail or all_tests_may_fail:
-        sys.stdout.write("ProcessingTest Test #{} - no leaks (but test failed)\n".format(i+1))
+        with open("cmd.out", "r") as f:
+            cmdout = f.read().strip()
+        sys.stdout.write("ProcessingTest Test #{} - no leaks (but test failed)\n{}\n".format(i+1, cmdout))
+        tests_that_failed.append(i+1)
     else:
         sys.stdout.write("Command '{}' failed with return code '{}' and extra info {}\nSee file valgrind.out for details.\n".format(cmd,memcheck.RetCode_strings[rc], info))
         exit(0)
     memcheck.cleanup()
+if len(tests_that_failed) > 0:
+    sys.stdout.write("ProcessingTest: {} tests failed: {}\n".format(len(tests_that_failed), tests_that_failed))
